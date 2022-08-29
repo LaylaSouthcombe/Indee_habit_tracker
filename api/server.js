3775eb862a5310db37e4f510e5996c1b11f5a907
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-08-29 13:13:04 +0100

    install node schedule and add job to create entry for each habit every day

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-
+const schedule = require('node-schedule');
 const server = express();
 server.use(cors());
 server.use(express.json());
+
+const db = require('./dbConfig/init');
 
 const usersRoutes = require('./routes/users')
 const carersRoutes = require('./routes/carers')
@@ -23,5 +25,46 @@ server.use('/auth', authRoutes)
 
 
 server.get('/', (req, res) => res.send('Welcome to Indee'))
+
+
+//CREATE NEW ENTRY FOR EACH USER EVERYDAY
+
+  
+async function addHabitEntryForEveryUser() {
+    return new Promise (async (resolve, reject) => {
+        try {
+            const users = await db.query(`SELECT * FROM users;`)
+            for (i = 0; i < users.rows.length; i++) {
+                //get habits info
+                //for length, if type = "int", add entry to int_entries
+                //if type = "boolean", add entry to boolean_entries
+                console.log("user id", users.rows[i].id)
+                const habitsInfo = await db.query('SELECT * FROM habits_info WHERE user_id = $1', [users.rows[i].id]);
+                console.log("habitsInfo", habitsInfo.rows)
+                for(let j = 0; j < habitsInfo.rows.length; j++){
+                    if(habitsInfo.rows[j].type === "int"){
+                        await db.query(`INSERT INTO int_entries (habit_int_id) VALUES ($1);`, [habitsInfo.rows[j].id]);
+                        console.log("data entered for int habit", habitsInfo.rows[j].id)
+                    }
+                    if(habitsInfo.rows[j].type === "boolean"){
+                        await db.query(`INSERT INTO boolean_entries (habit_bln_id) VALUES ($1);`, [habitsInfo.rows[j].id]);
+                        console.log("data entered for bln habit", habitsInfo.rows[j].id)
+                    }
+                }
+            console.log(`user entries created for ${users.rows[i].id}`)    
+            }
+            
+            resolve(`User entries created for users`);
+        } catch (err) {
+            console.log(err);
+            reject('Entry could not be created');
+        }
+    })
+}
+
+// const job = schedule.scheduleJob('*/1 * * * *', function(){
+//     addHabitEntryForEveryUser()
+// });
+
 
 module.exports = server
